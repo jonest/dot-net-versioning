0dc17ddef65b9e422459017057efdbd25feb5d0e
Ty Jones <ty.jones@healthcatalyst.com>
2017-06-03 23:48:32 -0600

    + major-minor tests

--- Tests/_suite.ts
+++ Tests/_suite.ts
@@ -72,4 +72,38 @@ describe("VersionAssemblies", function() {
             done();
         });
     });
+
+    describe("major-minor", function() {
+        it("should version correctly", (done: MochaDone) => {
+            // Arrange
+            let testPath: string = Path.join(__dirname, "VersionAssemblies" ,"major-minor.test.js");
+            let testRunner: MockTest.MockTestRunner = new MockTest.MockTestRunner(testPath);
+
+            // Act
+            testRunner.run();
+
+            // Assert
+            Assert(testRunner.succeeded, "versioning should have succeeded");
+            Assert.equal(testRunner.warningIssues.length, 0, "should have no warnings");
+            Assert.equal(testRunner.errorIssues.length, 0, "should have no errors");
+
+            done();
+        });
+
+        it("should version with info suffix correctly", (done: MochaDone) => {
+            // Arrange
+            let testPath: string = Path.join(__dirname, "VersionAssemblies" ,"major-minor-suffix.test.js");
+            let testRunner: MockTest.MockTestRunner = new MockTest.MockTestRunner(testPath);
+
+            // Act
+            testRunner.run();
+
+            // Assert
+            Assert(testRunner.succeeded, "versioning should have succeeded");
+            Assert.equal(testRunner.warningIssues.length, 0, "should have no warnings");
+            Assert.equal(testRunner.errorIssues.length, 0, "should have no errors");
+
+            done();
+        });
+    });
 });
